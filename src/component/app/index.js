9d44a823d7a401fef59f176bc1cfdada224f953e
Nick Asmussen <nick.asmussen1@gmail.com>
2018-05-13 17:50:44 -0700

    adds a hello world to app

---
+++ src/component/app/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+class App extends React.Component {
+  render() {
+    return(
+      <section>
+      <h1>Hello world</h1>
+    </section>
+    )
+  }
+}
+export default App;
